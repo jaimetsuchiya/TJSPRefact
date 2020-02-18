138c37f78de0ed75ddb1c9b4e668499b48ef7f18
Jaime R. Tsuchiya <jaime.tsuchiya@ironmountain.com.br>
2020-02-18 07:27:21 -0300

    Adicao do projeto de teste de rota do graphql

---
+++ src/Tests/GraphQLRoutingTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphQLRoutingTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
